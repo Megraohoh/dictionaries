7d5bb8c8941b157228420de1e9c748633ec4824f
Meghan Debity <mdebity@gmail.com>
2018-07-09 13:49:59 -0500

    created dictionary and list. pending report building

---
+++ stocks.py
@@ -0,0 +1,5 @@
+# Create a simple dictionary with ticker symbols and company names.
+stock_dictionary = { 'AAT': 'American Assets Trust', 'ABR': 'Arbor Realty Trust Inc', 'ABX': 'Barrick Gold Corp'}
+
+# Create a simple list of blocks of stock. (ticker, number of shares, purchased date, price)
+purchases = [('AAT', 100, '2-apr-1999', 48), ('ABR', 101, '4-may-2000', 24), ('ABX', 102, '5-jun-2000', 56)]
